76510f7ec24808b8e4898c1294e1515bc7470915
shinamia <920721922@qq.com>
2020-11-08 21:46:09 +0800

    forth

---
+++ PriestsAndDeivls2/Assets/Scripts/RoleModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoleModel// : MonoBehaviour
+{
+
+    public GameObject role;
+    public bool isPriest;
+    public int tag;
+    public bool isRight;
+    public bool isInBoat;
+    public Vector3 rightPos;
+    public Vector3 leftPos;
+
+    public RoleModel(bool isPriest,int tag){
+        this.isPriest=isPriest;
+        isRight=false;
+        this.tag = tag;
+        isInBoat=false;
+        rightPos=new Vector3(9,0,-33.3f+tag*1.1f);
+        leftPos=new Vector3(1,0,-33.3f+tag*1.1f);
+        role=GameObject.Instantiate(Resources.Load(isPriest?"Priests":"Devils", typeof(GameObject))) as GameObject;
+        role.transform.position=leftPos;
+        role.name = "role" + tag;
+        role.AddComponent<Click>();
+        role.AddComponent<BoxCollider>();
+        role.GetComponent<BoxCollider>().size=new Vector3(0.6f,3,0.6f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PriestsAndDeivls2/Assets/Scripts/UserGUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserGUI : MonoBehaviour {
+
+	private IUserAction action;
+	public string gameMessage;
+
+	void Start () {
+		action = SSDirector.getInstance ().currentSceneController as IUserAction;
+	}
+
+	void OnGUI() {  
+		float width = Screen.width / 6;  
+		float height = Screen.height / 12;
+
+		action.Check();
+		GUIStyle style = new GUIStyle();
+        style.normal.textColor = Color.red;
+        style.fontSize = 50;
+		GUI.Label(new Rect(325,120,50,200),gameMessage,style);
+
+		if (GUI.Button(new Rect(0, 0, width, height), "Restart")) {  
+			action.Restart();  
+		} 
+
+		string paused_title = SSDirector.getInstance ().Paused ? "Resume" : "Pause!"; 
+		if (GUI.Button(new Rect(width, 0, width, height), paused_title)) { 
+			SSDirector.getInstance ().Paused = !SSDirector.getInstance ().Paused;
+		} 
+	}
+
+
+}
